557bd6bdf2c0ef09abb03768d1fe4163e8c723f9
Sansthita-Dey <sansthitadey2905@gmail.com>
2026-06-05 10:14:53 +0530

    Added video PPE compliance analysis pipeline

--- src/detect_video.py
+++ src/detect_video.py
@@ -1,8 +1,12 @@
 import cv2
 from ultralytics import YOLO
 
+from compliance_engine import check_compliance
+
+
 # Load trained model
 model = YOLO("runs/detect/train-2/weights/best.pt")
+
 
 video_path = "videos/test.mp4"
 
@@ -12,7 +16,12 @@ if not cap.isOpened():
     print("Error: Cannot open video.")
     exit()
 
+
 frame_count = 0
+
+# Store PPE detected across the entire video
+all_detected_ppe = set()
+
 
 while True:
 
@@ -23,12 +32,26 @@ while True:
 
     frame_count += 1
 
+    # Process every 5th frame
     if frame_count % 5 != 0:
-     continue
+        continue
 
     # YOLO Detection
     results = model(frame, verbose=False)
 
+    # Collect detected PPE
+    for result in results:
+
+        for box in result.boxes:
+
+            class_id = int(box.cls[0])
+
+            class_name = model.names[class_id]
+
+            if class_name not in ["Person", "none"]:
+                all_detected_ppe.add(class_name)
+
+    # Draw detections
     annotated_frame = results[0].plot()
 
     cv2.putText(
@@ -49,7 +72,43 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+
 cap.release()
 cv2.destroyAllWindows()
 
-print(f"Total Frames Processed: {frame_count}")
+
+print(f"\nTotal Frames Processed: {frame_count}")
+
+print("\nDetected PPE Across Entire Video:")
+print(list(all_detected_ppe))
+
+
+# Compliance Check
+department = "Steel Melting Shop"
+
+compliance_result = check_compliance(
+    department,
+    list(all_detected_ppe)
+)
+
+
+print("\n========== VIDEO COMPLIANCE REPORT ==========")
+
+print("Department:", compliance_result["department"])
+
+print("Required PPE:",
+      compliance_result["required"])
+
+print("Detected PPE:",
+      compliance_result["detected"])
+
+print("Missing PPE:",
+      compliance_result["missing"])
+
+print("Compliance:",
+      compliance_result["compliance"], "%")
+
+if compliance_result["allowed"]:
+    print("STATUS: ALLOWED")
+else:
+    print("STATUS: NOT ALLOWED")
